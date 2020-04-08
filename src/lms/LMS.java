1c43d4693ba2af92d7d1778ecd359da8d947a6fb
nirbhay47 <56075925+nirbhay47@users.noreply.github.com>
2020-04-08 13:57:23 +0530

    Add files via upload

---
+++ src/lms/LMS.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lms;
+
+/**
+ *
+ * @author MAHE
+ */
+public class LMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static loginpg1 p1=new loginpg1();
+    public static void main(String[] args) {
+        p1.show();
+    }
+    
+}
